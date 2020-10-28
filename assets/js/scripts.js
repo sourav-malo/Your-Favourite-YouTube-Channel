0df2557890f980d52b24592dd57ccab7154cc3aa
sujancse5bu <60531496+sujancse5bu@users.noreply.github.com>
2020-10-29 01:15:03 +0600

    Error Solved 1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,43 +1,39 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-
-
-
-  // For All Channel
-  fetch('api/read-channels.php')
-    .then(res => res.json())
-    .then(data => {
-      checkChannelCookieAndRender(data)
-    })
-    .catch(err => console.log(err))
+    fetch('api/read-channels.php')
+        .then(res => res.json())
+        .then(data => checkChannelCookieAndRender(data))
+        .catch(err => console.log(err))
 })
 
-function checkChannelCookieAndRender(channels) {
-  let channelCookieStatuses = new Array(0)
-  channels.data.forEach(channel => {
-    fetch('api/check-channel-cookie.php', {
-      method: 'POST',
-      body: JSON.stringify({ channel_id: channel.id })
+async function checkChannelCookieAndRender(channels) {
+    let channelCookieStatuses = new Array(channels.length)
+    let iCount = 0
+    channels.data.forEach(async channel => {
+        await fetch('api/check-channel-cookie.php', {
+                method: 'POST',
+                body: JSON.stringify({
+                    channel_id: channel.id
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                channelCookieStatuses[channel.id - 1] = data.status
+                if (++iCount == 15) renderChannels(channels, channelCookieStatuses)
+            })
+            .catch(err => console.log(err))
     })
-      .then(res => res.json())
-      .then(data => {
-        channelCookieStatuses.push(data.status)
-        if (channels.data.length === channelCookieStatuses.length) {
-          renderChannels(channels, channelCookieStatuses)
-        }
-      })
-      .catch(err => console.log(err))
-  })
 }
 
 
 
-function renderChannels(channels, cookieStatuses) {
-  channels.data.forEach((channel, index) => {
+async function renderChannels(channels, cookieStatuses) {
 
-    let ratingContainer = '';
-    if (cookieStatuses[index] == 'unset') {
-      ratingContainer = `<div>
+    channels.data.forEach((channel, index) => {
+        let ratingNANCheck = ''
+        let ratingContainer = ''
+        if (channel.rating) ratingNANCheck = Math.round(channel.rating * 10) / 10
+        if (cookieStatuses[index] == 'unset') {
+            ratingContainer = `<div>
                   <div class="give">
                     <i class="fas fa-star give-rating"></i>
                     <i class="fas fa-star give-rating"></i>
@@ -47,13 +43,13 @@ function renderChannels(channels, cookieStatuses) {
                   </div>
                 </div>
             `
-    }
-    const parser = new DOMParser();
-    const parsedDocument = parser.parseFromString(`
+        }
+        const parser = new DOMParser();
+        const parsedDocument = parser.parseFromString(`
           <div class="grid-item">
             <div class="card-container">
               <div class="upper-container">
-                <div class="discounts">${Math.round(channel.rating * 10) / 10}</div>
+                <div class="discounts">${ratingNANCheck}</div>
                 <div class="image-container">
                   <img  src=${channel.logo_url} />
                 </div>
@@ -70,45 +66,28 @@ function renderChannels(channels, cookieStatuses) {
                 </div>
               </div>
             </div>
-          </div>`, "text/html"); document.getElementById('grid').appendChild(parsedDocument.getElementsByTagName('div')[0], true);
-  })
-  setStarClickEvent();
-}
-
-function setStarClickEvent() {
-  document.querySelectorAll('.give').forEach(starContainer => starContainer.addEventListener('click', starCounter))
-}
-function setStarClickEvent() {
-  //rating selection
-  document.querySelector('.give').addEventListener('click', function (e) {
-    let action = 'add';
-    for (const i of this.children) {
-      i.classList[action]('active');
-      if (i === e.target) action = 'remove';
-    }
-  });
-  document.querySelectorAll('.give').forEach(function (starContainer, index) {
-    starContainer.addEventListener('click', (e) => {
-      let count = 1;
-      if (e.target.classList.contains('fas')) {
-        e.target.parentElement.id = 'clicked'; // added
-        let ratingCount = e.target.closest('div.grid-item').querySelector('div.discounts')
-        let currentStar = e.target;
-        while (currentStar.previousElementSibling) {
-          currentStar = currentStar.previousElementSibling
-          ++count
-        }
-        createChannelCookie(index + 1, count, ratingCount);
-        console.log(ratingCount.innerHTML)
-      }
-      //      console.log(count)
-      //      console.log(index);
-
-
-
-
+          </div>`, "text/html");
+        document.getElementById('grid').appendChild(parsedDocument.getElementsByTagName('div')[0], true);
     })
-  })
+    starCounter();
+}
+
+function starCounter() {
+    document.querySelectorAll('.grid-item').forEach((starContainer, index) => {
+        starContainer.addEventListener('click', (e) => {
+            let count = 1
+            if (e.target.classList.contains('fas')) {
+                e.target.parentElement.id = 'clicked';
+                let ratingCount = e.target.closest('div.grid-item').querySelector('div.discounts')
+                let currentStar = e.target
+                while (currentStar.previousElementSibling) {
+                    currentStar = currentStar.previousElementSibling
+                        ++count
+                }
+                createChannelCookie(index, count, ratingCount)
+            }
+        })
+    })
 }
 
 
@@ -116,61 +95,57 @@ function setStarClickEvent() {
 // These functions will be called after click event on star
 
 function createChannelCookie(channelId, rating, ratingCount) {
-  fetch('api/create-channel-cookie.php', {
-    method: 'POST',
-    body: JSON.stringify({ channel_id: channelId })
-  })
-    .then(res => res.json())
-    .then(data => {
-      if (data.status === 'set') {
-        createNewRating(channelId, rating, ratingCount);
-      }
-
-    })
-    .catch(err => console.log(err))
+    fetch('api/create-channel-cookie.php', {
+            method: 'POST',
+            body: JSON.stringify({
+                channel_id: channelId + 1
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            createNewRating(channelId, rating, ratingCount)
+        })
+        .catch(err => console.log(err))
 }
 
+
+
 function createNewRating(channelId, rating, ratingCount) {
-  fetch('api/create-rating.php', {
-    method: 'POST',
-    body: JSON.stringify({
-      channel_id: channelId,
-      rating: rating
-    })
-  })
-    .then(res => res.json())
-    .then(data => {
-      if (data.status === 'Rating Inserted') {
-        //hiding star container
-        document.querySelector('#clicked').style.visibility = 'hidden'; // added 
-        document.querySelector('#clicked').id = ''; // added
-        updateNewRating(channelId, ratingCount);
+    fetch('api/create-rating.php', {
+            method: 'POST',
+            body: JSON.stringify({
+                channel_id: channelId + 1,
+                rating: rating
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.status === 'Rating Inserted') {
+                document.querySelector('#clicked').style.visibility = 'hidden'; // added 
+                document.querySelector('#clicked').id = ''; // added
+                updateNewRating(channelId, ratingCount);
+            }
 
-
-
-      }
-
-    })
-    .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
 }
 
 
 
 function updateNewRating(channelId, ratingCount) {
-  fetch('api/read-single-channel.php', {
-    method: 'POST',
-    body: JSON.stringify({ channel_id: channelId })
-  })
-    .then(res => res.json())
-    .then(data => {
-      if (data.status === 'Channel Found') {
-        let newRating = data.data.rating;
-        ratingCount.innerText = `${ Math.round(newRating * 10) / 10 }`
-        // console.log(data.data.rating);
-        // have to update the rating in DOM
+    fetch('api/read-single-channel.php', {
+            method: 'POST',
+            body: JSON.stringify({
+                channel_id: channelId + 1
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.status === 'Channel Found') {
+                let newRating = data.data.rating;
+                ratingCount.innerText = `${ Math.round(newRating * 10) / 10 }`
+            }
 
-      }
-
-    })
-    .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
 }
